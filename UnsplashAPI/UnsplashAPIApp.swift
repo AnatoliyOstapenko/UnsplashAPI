8c2fe57d17bf540331e974d52928c7461c362f89
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2025-02-15 15:04:36 +0200

    Initial Commit

---
+++ UnsplashAPI/UnsplashAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAPIApp.swift
+//  UnsplashAPI
+//
+//  Created by Anatoliy Ostapenko on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
